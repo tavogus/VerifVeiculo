04b156fbfc3539f2b23df0acef0fe22b0ebde67b
Gustavo Becker <Gustavo Becker>
2016-03-08 23:21:40 -0300

    primeira versao

---
+++ src/com/android/verifveiculo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.android.verifveiculo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	private EditText edplaca;
+	private Button btverificar, btlimpar;
+	private TextView mostrarplaca;
+	private ImageView imgbandeira; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getComponets();
+		validarPlaca();
+		limparCampos();
+	}
+	
+	private void getComponets(){
+		edplaca = (EditText) findViewById(R.id.edplaca);
+		btverificar = (Button) findViewById(R.id.butverificar);
+		btlimpar = (Button) findViewById(R.id.butlimpar);
+		mostrarplaca = (TextView) findViewById(R.id.mostrarplaca);	
+		imgbandeira = (ImageView) findViewById(R.id.imgbandeira);
+
+	}
+
+	private void validarPlaca(){
+		btverificar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {  
+				VerificarUF uf = new VerificarUF();
+				if(edplaca.getText().toString().isEmpty()){
+					Toast.makeText(getApplicationContext(),"Informe a placa para validar",Toast.LENGTH_LONG).show();
+				}else{
+					String placa = edplaca.getText().toString();
+					
+					if(placa.matches("[a-zA-Z]{3,3}-\\d{4,4}")){
+						mostrarplaca.setText("Placa Válida, região: " + uf.getUF(placa));
+						mostrarplaca.setTextColor(Color.parseColor("#00FF7F"));
+					}else{
+						mostrarplaca.setText("Placa Inválida");
+						mostrarplaca.setTextColor(Color.parseColor("#FF0000"));
+					}
+				}	
+			}
+		});
+	}
+	private void limparCampos(){
+		btlimpar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				edplaca.setText("");
+				mostrarplaca.setText("");	
+			}
+		});
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/com/android/verifveiculo/VerificarUF.java
@@ -0,0 +1,66 @@
+package com.android.verifveiculo;
+
+public class VerificarUF {
+
+	String iniciais[] = new String[] { "NBB0001", "OHL0001", "MZN0001",
+			"NXR0001", "JWF0001", "NOI0001", "OAA0001", "NAH0001", "NUH0001",
+			"JTA0001", "NSE0001", "OBT0001", "OFI0001", "OSW0001", "NEI0001",
+			"MVL0001", "OLH0001", "HOL0001", "NHA0001", "NMP0001", "NWS0001",
+			"OIR0001", "LVF0001", "NHU0001", "ODU0001", "OUA0001", "HTX0001",
+			"NQL0001", "NUM0001", "OCB0001", "OHX0001", "ORN0001", "MXH0001",
+			"NNJ0001", "OJR0001", "MMN0001", "NPR0001", "OET0001", "OFX0001",
+			"KFD0001", "NXU0001", "PEE0001", "PFR0001", "PGL0001", "MUA0001",
+			"NLV0001", "OHB0001", "ORD0001", "HZB0001", "NVG0001", "OEJ0001",
+			"JKS0001", "NTD0001", "NYH0001", "OKI0001", "OUF0001", "GKJ0001",
+			"NXX0001", "OLO0001", "OOV0001", "MOX0001", "OCV0001", "OVE0001",
+			"KMF0001", "BFA0001", "SAV0001", "AAA0001", "LWR0001", "OWD0001",
+			"IAQ0001", "HQF0001", "NRF0001", "OOG0001", "JXZ0001", "NIY0001",
+			"NPC0001", "NTX0001", "OAP0001", "KAV0001", "NFC0001", "NJX0001",
+			"NVO0001", "OGH0001", "OMI0001", "JDP0001" };
+
+	String ufs[] = new String[] { "Rondônia", "Rondônia", "Acre", "Acre",
+			"Amazonas", "Amazonas", "Amazonas", "Roraima", "Roraima", "Pará",
+			"Pará", "Pará", "Pará", "Pará", "Amapá", "Tocantins", "Tocantins",
+			"Maranhão", "Maranhão", "Maranhão", "Maranhão", "Maranhão",
+			"Piauí", "Piauí", "Piauí", "Piauí", "Ceará", "Ceará", "Ceará",
+			"Ceará", "Ceará", "Ceará", "Rio Grande do Norte",
+			"Rio Grande do Norte", "Rio Grande do Norte", "Paraíba", "Paraíba",
+			"Paraíba", "Paraíba", "Pernambuco", "Pernambuco", "Pernambuco",
+			"Pernambuco", "Pernambuco", "Alagoas", "Alagoas", "Alagoas",
+			"Alagoas", "Sergipe", "Sergipe", "Sergipe", "Bahia", "Bahia",
+			"Bahia", "Bahia", "Bahia", "Minas Gerais", "Minas Gerais",
+			"Minas Gerais", "Minas Gerais", "Espírito Santo", "Espírito Santo",
+			"Espírito Santo", "Rio de Janeiro", "São Paulo", "São Paulo",
+			"Paraná", "Santa Catarina", "Santa Catarina", "Rio Grande do Sul",
+			"Mato Grosso do Sul", "Mato Grosso do Sul", "Mato Grosso do Sul",
+			"Mato Grosso", "Mato Grosso", "Mato Grosso", "Mato Grosso",
+			"Mato Grosso", "Goiás", "Goiás", "Goiás", "Goiás", "Goiás",
+			"Goiás", "Distrito Federal" };
+
+	String finais[] = new String[] { "NEH9999", "OHW9999", "NAG9999",
+			"NXT9999", "JXY9999", "NPB9999", "OAO9999", "NBA9999", "NUL9999",
+			"JWE9999", "NTC9999", "OCA9999", "OFW9999", "OTZ9999", "NFB9999",
+			"MXG9999", "OLN9999", "HQE9999", "NHT9999", "NNI9999", "NXQ9999",
+			"OJK9999", "LWQ9999", "NIX9999", "OEI9999", "OUE9999", "HZA9999",
+			"NRE9999", "NVF9999", "OCU9999", "OIQ9999", "OSV9999", "MZM9999",
+			"NOH9999", "OKC9999", "MOW9999", "NQK9999", "OFH9999", "OGG9999",
+			"KME9999", "NXW9999", "PFQ9999", "PGK9999", "PHE9999", "MVK9999",
+			"NMO9999", "OHK9999", "ORM9999", "IAP9999", "NVN9999", "OES9999",
+			"JSZ9999", "NTW9999", "NZZ9999", "OLG9999", "OVD9999", "HOK9999",
+			"NYG9999", "OMH9999", "ORC9999", "MTZ9999", "ODT9999", "OWC9999",
+			"LVE9999", "GKI9999", "SAV9999", "BEZ9999", "MMM9999", "OYG9999",
+			"JDO9999", "HTW9999", "NSD9999", "OOU9999", "KAU9999", "NJW9999",
+			"NPQ9999", "NUG9999", "OBS9999", "KFC9999", "NGZ9999", "NLU9999",
+			"NWR9999", "OHA9999", "OOF9999", "JKR9999" };
+
+	public  String getUF(String placa) {
+		for (int i = 0; i < iniciais.length; i++) {
+			if ((placa.compareToIgnoreCase(iniciais[i]) >= 0)
+			  && (placa.compareToIgnoreCase(finais[i]) <= 0)) {
+				return ufs[i];
+			}
+
+		}
+		return "Placa Invalida";
+	}
+}
